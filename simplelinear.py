9fa841f4d75f6a3f4998abdb292d3adb9f2c0e0e
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-20 20:23:08 +0300

    simple linear regression added

---
+++ simplelinear.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+#imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+#reading .csv file data
+data = pd.read_csv("hw_25000.csv")
+
+height = data.Height.values.reshape(-1,1)
+weight = data.Weight.values.reshape(-1,1)
+
+regression = LinearRegression()
+regression.fit(height,weight) #fitting the lines according to weight and height
+
+#sample predictions 
+print(regression.predict([[60]])) 
+print(regression.predict([[62]]))
+print(regression.predict([[64]]))
+print(regression.predict([[66]]))
+print(regression.predict([[68]]))
+print(regression.predict([[70]]))
+
+print(data.columns)
+
+plt.scatter(data.Height, data.Weight) #visualization
+x = np.arange(min(data.Height),max(data.Height)).reshape(-1,1) #create a range 
+plt.plot(x,regression.predict(x),color="red") #drawing predictions' line, y->x's predicted value
+plt.xlabel("Height")
+plt.ylabel("Weight")
+plt.title("Simple Linear Regression Model")
+plt.show()
