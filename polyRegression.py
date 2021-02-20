ca28742677f0e2a36277dd067e0ee32a10f63b62
duygueroglu <45760467+duygueroglu@users.noreply.github.com>
2021-02-21 00:15:42 +0300

    Update polyRegression.py
    second regression prediction added

--- polyRegression.py
+++ polyRegression.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 data = pd.read_csv("positions.csv")
 print(data.columns)
@@ -16,6 +17,15 @@ regression.fit(level,salary)
 
 prediction = regression.predict([[8.3]])
 
+regressionPoly = PolynomialFeatures(degree = 4)
+levelPoly = regressionPoly.fit_transform(level)
+regression2 = LinearRegression()
+regression2.fit(levelPoly,salary)
+
+prediction2 = regression2.predict(regressionPoly.fit_transform([[8.3]]))
+
+
 plt.scatter(level,salary,color="red")
 plt.plot(level,regression.predict(level),color="blue")
+plt.plot(level,regression2.predict(levelPoly))
 plt.show()
